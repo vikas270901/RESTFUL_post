1c1d750b907e3dc5efd2c9b5e1c34dfe2912f6ba
vikas270901 <38756061+vikas270901@users.noreply.github.com>
2019-02-23 23:36:00 +0530

    Add files via upload

---
+++ models/profile.js
@@ -0,0 +1,15 @@
+var mongo = require("mongoose");
+
+var schema = new mongo.Schema({
+		ide: String,
+		name: String,
+		gender: String,
+		image:String,
+		work: String,
+		home: String,
+		contact: String,
+		dob: String,
+		about: String,
+		status : {type: Number, default: 1}
+});
+module.exports = new mongo.model("profile", schema);
